947e8a8ee8fb3973a4ebfe3f8b6dfb242aaa3aec
wendao2004 <1601261772@qq.com>
2026-09-13 14:59:51 +0800

    提取组件

--- commen/utils/utils.js
+++ commen/utils/utils.js
@@ -39,3 +39,35 @@ export function switchTab(url) {
 		}
 	})
 }
+
+export function reLaunch(url) {
+	uni.reLaunch({
+		url: url,
+		success: (res) => {
+			console.log("跳转成功", res);
+			console.log("跳转至", url);
+		},
+		fail: (err) => {
+			console.log(err);
+		}
+	})
+}
+
+export function showToast(title, icon) {
+	uni.showToast({
+		title: title,
+		icon: icon
+	})
+}
+export function hideToast() {
+	uni.hideToast();
+}
+
+export function showLoading(title) {
+	uni.showLoading({
+		title: title
+	})
+}
+export function hideloading() {
+	uni.hideLoading();
+}
